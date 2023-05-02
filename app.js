b6c380422258597c481a3dca99019e4bc3ded073
rehman258 <rehman1998eliyev@gmail.com>
2023-05-03 00:14:49 +0400

    working on slider config

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 class Slider {
     constructor(props){
         this.slider;
+        this.sliderWidth;
+        
+        this.lens;
+        this.lensPosition;
+        this.lensStart;
+        this.lensEnd;
+
         this.sliderItems; 
         this.settings;
-        this.lens;
 
         this.leftClones=[];
         this.rightClones=[];
@@ -14,30 +20,43 @@ class Slider {
 
 
     _configSlider(){
+        this.sliderItemWidth = this.slider.clientWidth;
+        // console.log(this.lensEnd)
+
         this.lens = document.createElement('div');
         this.lens.classList.add('slider-lens');
         // clone element both sides for appding to before and end 
         this.sliderItems.forEach((item,i)=>{
             if((this.sliderItems.length/i) < i){
                 let cloneItem = item.cloneNode(true);
-                cloneItem.classList.add('cloned')
-
+                cloneItem.classList.add('cloned');
                 this.leftClones.push(cloneItem);
             }else{
                 let cloneItem = item.cloneNode(true);
-                cloneItem.classList.add('cloned')
-                this.rightClones.push(cloneItem)
+                cloneItem.classList.add('cloned');
+                this.rightClones.push(cloneItem);
             }
         })
 
         // collection all slider element in one place
         this.sliderItems = this.leftClones.concat(this.sliderItems,this.rightClones)
         this.slider.innerHTML='';
-
         this.sliderItems.forEach(sliderItem=>{
+            sliderItem.style.width=`${this.sliderItemWidth}px`;
             this.lens.append(sliderItem);
         })
+
+
+        // lens start at this position , calculation here cause of after cloned before elements
+        /* working here down */
+        this.lensStart=-this.leftClones.length*this.sliderItemWidth;
+        this.lensEnd = this.sliderItems.length * this.sliderItemWidth;
+        
+        this.lens.style.transform= `translate3d(${this.lensStart}px,0px,0px)` 
+        
         this.slider.append(this.lens)
+        /* working here up */
+        
 
 
         // if buttons are true
@@ -48,6 +67,10 @@ class Slider {
         this._buttonEvents()
     }
     
+    _settingApllier(settings){
+
+    }
+
     init(containerClass,settings){
         this.slider = document.querySelector(containerClass);
         this.settings=settings;
@@ -57,15 +80,22 @@ class Slider {
     
     _buttonEvents(){
         this.prevBtn.addEventListener('click',()=>{
-            console.log('prev')
+            this._lensMovement('prev')
         })
         this.nextBtn.addEventListener('click',()=>{
-            console.log('next')
+            this._lensMovement('next')
         })
-
     }
 
-    _lensMovement(){
+
+    _lensMovement(lensDir){
+        let lensLocal = window.getComputedStyle(this.lens).getPropertyValue('transform').split(','); 
+        if(lensDir==='next'){
+            console.log(this.lensEnd)
+            this.lens.style.transform = `translate3d(${lensLocal[lensLocal.length-2]-this.sliderItemWidth}px,0px,0px)`
+        }else{
+            this.lens.style.transform = `translate3d(${parseInt(lensLocal[lensLocal.length-2])+this.sliderItemWidth}px,0px,0px)`
+        }
     }
 
     _startPosition(){
